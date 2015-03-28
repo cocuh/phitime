4067ae79be0917ed58b0c9a77d1e075a673193fd
cocuh <cocuh.kk@gmail.com>
2015-03-29 03:58:26 +0900

    implement secret init reader

--- phitime/__init__.py
+++ phitime/__init__.py
@@ -12,6 +12,21 @@ required_plugins = [
 ]
 
 
+def _load_secret_ini(settings):
+    import six.moves
+
+    secret_ini = settings.get("phitime.secret_ini", None)
+    if secret_ini is None:
+        return
+
+    config = six.moves.configparser.ConfigParser()
+    config.read([secret_ini])
+
+    for section in settings['phitime.secret.sections'].strip().splitlines():
+        settings.update(config.items(section))
+    return settings
+
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
@@ -25,6 +40,12 @@ def main(global_config, **settings):
 
     config.add_static_view('static/bootstrap', 'resources/bootstrap/dist', cache_max_age=3600)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    
+    config.add_route('top', '/')
+    config.add_route('event.create', '/event_create')
+    config.add_route('event.detail', '/event/{scrambled_event_id}/')
+    config.add_route('event.edit', '/event/{scrambled_event_id}/edit')
+    config.add_route('member.edit', '/event/{scrambled_event_id}/{member_position}/edit')
+
     config.scan()
     return config.make_wsgi_app()
