41bc527d34db27ae2a892da76db857bcd4c791f8
cocuh <cocuh.kk@gmail.com>
2015-03-29 03:58:53 +0900

    add view scafolds

--- phitime/views.py
+++ phitime/views.py
@@ -1,8 +1,59 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'phitime'}
+class TopView(object):
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(route_name='top', renderer='templates/top.jinja2')
+    def top(self):
+        return {}
+
+
+class UserView(object):
+    def __init__(self, request):
+        self.request = request
+
+
+class EventView(object):
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(route_name='event.create', request_method='GET', renderer='templates/event/create.jinja2')
+    def create_get(self):
+        pass
+
+    @view_config(route_name='event.create', request_method='POST')
+    def create_post(self):
+        pass
+
+    @view_config(route_name='event.edit', request_method='GET', renderer='templates/event/edit.jinja2')
+    def edit_get(self):
+        pass
+
+    @view_config(route_name='event.edit', request_method='POST')
+    def edit_post(self):
+        pass
+
+    @view_config(route_name='event.detail', request_method='GET', renderer='tempaltes/event/detail.jinja2')
+    def detail_get(self):
+        pass
+
+    @view_config(route_name='event.detail', request_method='POST')
+    def detail_post(self):
+        pass
+
+
+class MemberView(object):
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(route_name='member.edit', request_method='GET', renderer='templates/member/edit.jinja2')
+    def edit_get(self):
+        pass
+
+    @view_config(route_name='member.edit', request_method='POST')
+    def edit_post(self):
+        pass
 
 
