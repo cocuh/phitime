83e005d5a57ef2ebe985dcd4181a42b18c4bb51e
cocuh <cocuh.kk@gmail.com>
2015-04-25 22:10:07 +0900

    improve document and fix some bugs

--- phitime/timetable.py
+++ phitime/timetable.py
@@ -1,29 +1,35 @@
 class TimetableType(object):
+    """manage all TimetableType.
+    """
+
     timetable_types = []
+    """:type: list[TimetableType]"""
     default_type = None
-    
-    def __init__(self, name, route_name, display_name_jp, display_name_en):
+    """:type: TimetableType"""
+
+    def __init__(self, name, route_name, display_name):
         self.name = name
-        self.display_name_jp = display_name_jp
-        self.display_name_en = display_name_en
+        self.display_name = display_name
         self.route_name = route_name
-    
+
     @classmethod
     def find_by_name(cls, name):
         for timetable in cls.timetable_types:
             if timetable.name == name:
                 return timetable
         return None
-    
+
     @classmethod
-    def register(cls, name, route_name, display_name_jp, display_name_en):
-        timetable_type = cls(name, route_name, display_name_jp, display_name_en)
+    def register(cls, name, route_name, display_name):
+        timetable_type = cls(name, route_name, display_name)
+
+        cls.timetable_types.append(timetable_type)
         
         if cls.default_type is None:
             cls.set_default(timetable_type)
-        
+
         return timetable_type
-    
+
     @classmethod
     def set_default(cls, default_type):
         """
@@ -32,9 +38,8 @@ class TimetableType(object):
         """
         cls.default_type = default_type
 
-TimetableType.register('univ_tsukuba', 'timetable.univ_tsukuba',
-    '筑波大学時間割', 'timetable of Univ.Tsukuba')
 
-TimetableType.register('half_hourly', 'timetable.half_hourly',
-    '30分区切り', 'half hourly timetable')
+TimetableType.register('univ_tsukuba', 'timetable.univ_tsukuba', '筑波大学時間割')
+
+TimetableType.register('half_hourly', 'timetable.half_hourly', '30分区切り')
 
