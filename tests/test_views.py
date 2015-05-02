355640f55795b8a63810b4ae99591ad3c2796d71
cocuh <cocuh.kk@gmail.com>
2015-05-03 02:31:55 +0900

    update routing of test

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,5 @@
 from pyramid.testing import DummyRequest
 from . import BaseTestCase
-import transaction
 from phitime.models import Event
 
 
@@ -10,13 +9,15 @@ class BaseViewTestCase(BaseTestCase):
         self._init_route()
 
     def _init_route(self):
-        self.config.add_route('top', '/')
-        self.config.add_route('event.create', '/event_create')
-        self.config.add_route('event.detail', '/event/{event_scrambled_id}/')
-        self.config.add_route('event.edit', '/event/{event_scrambled_id}/edit')
-        self.config.add_route('member.edit', '/event/{event_scrambled_id}/{member_position}/edit')
-        self.config.add_route('timetable.univ_tsukuba', '/timetable/univ_tsukuba.svg')
-        self.config.add_route('timetable.half_hourly', '/timetable/half_hourly.svg')
+        config = self.config
+        config.add_route('top', '/')
+        config.add_route('event.create', '/event_create')
+        config.add_route('event.detail', '/event/{event_scrambled_id}/')
+        config.add_route('event.edit', '/event/{event_scrambled_id}/edit')
+        config.add_route('member.create', '/event/{event_scrambled_id}/create_member')
+        config.add_route('member.edit', '/event/{event_scrambled_id}/{member_position}/edit')
+        config.add_route('timetable.univ_tsukuba', '/timetable/univ_tsukuba.svg')
+        config.add_route('timetable.half_hourly', '/timetable/half_hourly.svg')
 
     def _make_event(self, name=u'いべんとなめ', description=u'ですくりぷしょん', timetable_type='half_hourly'):
         event = Event(name, description, timetable_type)
